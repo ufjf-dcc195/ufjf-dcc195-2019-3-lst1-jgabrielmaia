08b8db9fbca7e96692c1034c04a2abdb69bf7919
J. Gabriel Maia <jonagabriel@gmail.com>
2019-09-07 19:28:36 -0300

    Adding primes action

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const url = require('url');
 
 exports.index = (req, res) =>  {
     res.writeHead(200, {"Content-Type":"text/html"});
-    res.write("<h1>Hello World!</h1>");
+    res.write("<h1>Hello Node!</h1>");
     res.end();
 }
 
@@ -17,6 +18,10 @@ exports.about = (req, res) =>  {
     res.writeHead(200, {"Content-Type":"text/html"});
     res.write(content);
     res.end();
+}
+
+randomNumberBetween = (start, end) => {
+    return Math.floor(Math.random() * (start - end + 1) + end);
 }
 
 exports.random = (req, res) => {
@@ -43,10 +48,73 @@ exports.random = (req, res) => {
     odds.map((x) => res.write("<div>" + x + "</div>"));
     res.write("</div>");
     
-    res.end();
-    
+    res.end();    
 }
 
-randomNumberBetween = (start, end) => {
-    return Math.floor(Math.random() * (start - end + 1) + end);
+exports.primes = (req, res) => {
+    res.writeHead(200, {"Content-Type":"text/html"});
+    let params = url.parse(req.url, true).query;
+    let start = params.n1;
+    let end = params.n2;
+
+    if(!(start || end )){
+        res.write("<h2>Please, provide two numbers on your query.");
+        res.end();
+    }
+
+    if(!(start < end && end < 100)){
+        res.write("<h2>Numbers out of bounds. Rule: N1 < N2 < 100");
+        res.end();
+    }
+    console.log(start);
+    let primes = calculatePrimesForRange(start, end);
+    primes.map((x) => res.write("<div>" + x + "</div>"));  
+
+    res.end();
+}
+
+// source code: https://snippets.bentasker.co.uk/page-1705211104-Find-Prime-Numbers-in-Range-Javascript.html
+
+function calculatePrimesForRange(l,h){
+    var primes=[];
+    for (i=l; i<h; i++){
+
+        if (checkNumPrime(i)){
+          primes.push(i);
+        }
+
+    }
+    return primes;
+}
+
+// Check whether a number is a prime
+function checkNumPrime(n){
+
+    // Check whether the number is 1,2 or 3
+    if (n<4){
+        return true;
+    }
+
+    // Check the number isn't directly divisible by 2 or 3
+    if (n%2 == 0 || n%3 == 0){
+        return false;
+    }
+
+    var di=2;
+    var i = 5;
+
+    // Don't calculcate higher than the square root (rounded down if needed)
+    var lim = Math.floor(Math.sqrt(n));
+
+    while (i < lim){
+
+      if (n%i == 0){
+          return false;
+      }
+      i=i+di;
+      di=6-di;
+    }
+
+    // If we haven't already returned, n is prime
+    return true;
 }
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ routes["/"] = controllers.index;
 routes["/index.html"] = controllers.index;
 routes["/sobre.html"] = controllers.about;
 routes["/aleatorio.html"] = controllers.random;
+routes["/primos.html"] = controllers.primes;
 routes["404"] = controllers.notFound;
 
 server.start(router.route, routes);
