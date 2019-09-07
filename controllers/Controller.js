447640e3ce287e5ee95c5b13e76beb45af5ab29f
J. Gabriel Maia <jonagabriel@gmail.com>
2019-09-06 23:46:19 -0300

    Adding about view and controller action

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,11 +1,19 @@
-exports.index = function (req, res) {
+var fs = require('fs');
+
+exports.index = (req, res) =>  {
     res.writeHead(200, {"Content-Type":"text/html"});
     res.write("<h1>Hello World!</h1>");
     res.end();
 }
 
-exports.notFound = function (req, res) {
+exports.notFound = (req, res) => {
     res.writeHead(404, {"Content-Type":"text/html"});
     res.write("<h1>404 Not Found</h1>");
+    res.end();
+}
+
+exports.about = (req, res) =>  {
+    var content = fs.readFileSync(__dirname + '/../pages/about.html');
+    res.write(content);
     res.end();
 }
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const controllers = require('./controllers/Controller.js');
 
 const routes = {};
 routes["/"] = controllers.index;
+routes["/index.html"] = controllers.index;
+routes["/sobre.html"] = controllers.about;
 routes["404"] = controllers.notFound;
 
 server.start(router.route, routes);
