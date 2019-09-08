a6bcfc0fc00c7b293417d74e93e0d2624771e3cd
J. Gabriel Maia <jonagabriel@gmail.com>
2019-09-08 13:41:07 -0300

    Adding heroku configuration

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ function start(route, routes) {
     let server = http.createServer(function(req, res) {
         route(url.parse(req.url).pathname, routes, req, res); 
     });
-    server.listen(8880);
-
-    console.log("Servidor na porta http://localhost:8880");
+    server.listen(process.env.PORT || 8880);
 }
 
 exports.start = start;
